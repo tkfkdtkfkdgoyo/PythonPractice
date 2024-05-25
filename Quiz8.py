50ae86801fe513ceef92d0b805172630f512ede9
jumalgom <jo_god_da@inha.edu>
2024-05-25 21:37:18 +0900

    feat.퀴즈 7까지 해결

---
+++ Quiz8.py
@@ -0,0 +1,112 @@
+def load_file(): # 파일을 읽어서 리스트로 반환
+    students = []
+    try:
+        with open('student.txt', "r") as file:
+            for line in file:
+                name, age, score = line.strip().split(',')
+                students.append({'name' : name, 'age' : int(age), 'score' : float(score)})
+    except FileNotFoundError: # 첫번째 에러 처리 방법
+        print("파일을 찾을 수 없습니다. 새로운 파일을 생성합니다.")
+        open("student.txt", "w").close()
+    return students
+
+def save_file(students): # 리스트를 파일에 저장
+    with open('student.txt', 'w') as file:
+        for student in students:
+            file.write(f"{student['name']},{student['age']},{student['score']}\n")
+
+def add(students):
+    load_file()
+    new_name = input('추가할 데이터의 이름을 입력하세요: ')
+
+    try:
+        new_age = int(input('추가할 데이터의 나이를 입력하세요: '))
+    except ValueError: # 두번째 에러 처리 방법
+        print('정수를 입력해주세요.')
+        return
+
+    if new_age < 0:
+        print("나이는 음수일 수 없습니다.")
+        return
+
+    try:
+        new_score = float(input('추가할 데이터의 점수를 입력하세요: '))
+    except ValueError:
+        print('실수를 입력해주세요.')
+        return
+
+    if new_score < 0:
+        print("성적은 음수일 수 없습니다.")
+        return
+
+    if type(new_age) != int or type(new_score) != float:
+        print('잘못된 입력입니다.')
+        return
+
+    students.append({'name' : new_name, 'age' : new_age, 'score' : new_score})
+    print('추가됨')
+    save_file(students)
+
+def search(students):
+    load_file()
+    search_input = input('검색할 데이터의 키와 값을 key:value 형태로 입력하세요: ')
+    try:
+        key, value = search_input.split(':')
+        if key not in ['name', 'age', 'score']:
+            raise KeyError
+
+        for student in students:
+            if student[key] == value:
+                print(student)
+                return
+        print('데이터를 찾을 수 없습니다.')
+
+    except KeyError: # 세번째 에러 처리 방법
+        print('잘못된 key입니다. name, age, score 중 하나를 입력해주세요.')
+
+def delete(students):
+    load_file()
+    try:
+        if len(students) == 0:
+            raise IndexError
+    except IndexError: # 네번째 에러 처리 방법
+        print('데이터가 없습니다.')
+        return
+
+    delete_input = input('삭제할 데이터의 이름을 입력하세요: ')
+    for student in students:
+        if student['name'] == delete_input:
+            students.remove(student)
+            print('삭제됨')
+            save_file(students)
+            return
+    print('데이터를 찾을 수 없습니다.')
+
+
+def main():
+    students = load_file()
+
+    while True:
+        print("간단한 데이터 베이스 구현")
+        print("1. 데이터 추가")
+        print("2. 데이터 검색")
+        print("3. 데이터 삭제")
+        print("4. 종료")
+        user_input = int(input("메뉴를 선택하세요 > "))
+
+        if user_input == 1:
+            try:
+                add(students)
+            except IndexError:
+                print("입력이 부족합니다.")
+        elif user_input == 2:
+            search(students)
+        elif user_input == 3:
+            delete(students)
+        elif user_input == 4:
+            print('프로그램을 종료합니다.')
+            break
+        else:
+            print('잘못된 입력입니다.')
+
+main()
